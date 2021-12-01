f4aa7133ec1b56fec9f4f8470becf42a3a572034
samikhawja <skhawja11@gmail.com>
2021-12-01 14:17:24 -0800

    journalSchema complete

---
+++ server/models/Journal.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require('mongoose');
+// const dateFormat = require('../utils/dateFormat');
+
+const journalSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+        maxlength: 50,
+    },
+    body: {
+        type: String,
+        required: true,
+        trim: true,
+        maxlength: 1000,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        // get: (timestamp) => dateFormat(timestamp),
+    },
+});
+
+const Journal = model('Journal', journalSchema);
+
+module.exports = Journal;
---
+++ server/utils/dateFormat.js
@@ -0,0 +1,1 @@
+// moment for fortmatting
