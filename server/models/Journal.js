8aaf22025f92daad69d5e550adfa356a29e9972a
Sarah Hollingsworth <sahhollingsworth@gmail.com>
2021-12-02 15:50:21 -0800

    added Sentiment to Journal model

--- server/models/Journal.js
+++ server/models/Journal.js
@@ -14,6 +14,10 @@ const journalSchema = new Schema({
         trim: true,
         maxlength: 1000,
     },
+    sentiment: {
+        type: String,
+        required: true,
+    },
     createdAt: {
         type: Date,
         default: Date.now,
