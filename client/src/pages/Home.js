efada0330ded4326fc2d8e37b9476f9420d5c0a5
Sarah Hollingsworth <sahhollingsworth@gmail.com>
2021-12-02 15:11:30 -0800

    Added placeholder function & export to Home page so app will build

---
+++ client/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// Placeholder so the app will build
+const Home = () => {
+    return (
+        <div>
+            <h1>HOME page placeholder</h1>
+        </div>
+    );
+};
+
+export default Home;
