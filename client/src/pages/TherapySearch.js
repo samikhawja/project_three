93f9fe00a285e352faabda5fe1bcb4c987c77774
Saraah Hollingsworth <sahhollingsworth@gmail.com>
2021-12-03 11:37:37 -0800

    Temp commented out to enable app build
    Updated API call logic to use api util

--- client/src/pages/TherapySearch.js
+++ client/src/pages/TherapySearch.js
@@ -1,44 +1,72 @@
 import React, { useState, useEffect } from 'react';
-require('dotenv').config();
-
-console.log(process.env);
+// // External fetch to the google books api endpoint still managed in utils/API, not server implementation
+// import { searchGooglePlaces } from '../utils/api';
 
 function TherapySearch() {
-    const [error, setError] = useState(null);
-    const [isLoaded, setIsLoaded] = useState(false);
-    const [items, setItems] = useState([]);
+    // const [error, setError] = useState(null);
+    // const [isLoaded, setIsLoaded] = useState(false);
+    // const [items, setItems] = useState([]);
+
+    // const handleFormSubmit = async (event) => {
+    //   event.preventDefault();
+      
+    //   // Catch if there wasn't any data provided by the user in the search form
+    //   if (!searchData) {
+    //     return false;
+    //   }
     
-    useEffect(() => {
-        const api_key = process.env.API_KEY;
-        fetch(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=37.759185,-122.427304&radius=40235&types=doctor&name=addiction_therapy&key=${api_key}`)
-            .then(res => res.json())
-            .then(
-            (result) => {
-                setIsLoaded(true);
-                setItems(result);
-            },
-            (error) => {
-                setIsLoaded(true);
-                setError(error);
-            }
-            )
-    }, [])
+    //   try {
+    //     // pass through search field data including location and types
+    //     const response = await searchGoogleAPI(searchData);
+
+    //     if (!response.ok) {
+    //       throw new Error('We encountered an error completing your search.')
+    //     }
+
+    //     // convert api response to a JSON object
+    //     const { items } = await response.json();
+
+    //     //placeholder to avoid error
+    //     console.log(items);
+
+    //   }
+    // },
+    //Commented this out while we continue work on the API logic, so that the abb will build in the meanwhile
+    // useEffect(() => {
+        
+    //         .then(res => res.json())
+    //         .then(
+    //         (result) => {
+    //             setIsLoaded(true);
+    //             setItems(result);
+    //         },
+    //         (error) => {
+    //             setIsLoaded(true);
+    //             setError(error);
+    //         }
+    //         )
+    // }, [])
   
-    if (error) {
-      return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
-      return <div>Loading...</div>;
-    } else {
-      return (
-        <ul>
-          {items.map(item => (
-            <li key={item.id}>
-              {item.name} {item.price}
-            </li>
-          ))}
-        </ul>
-      );
-    }
+    // if (error) {
+    //   return <div>Error: {error.message}</div>;
+    // } else if (!isLoaded) {
+    //   return <div>Loading...</div>;
+    // } else {
+    //   return (
+    //     <ul>
+    //       {items.map(item => (
+    //         <li key={item.id}>
+    //           {item.name} {item.price}
+    //         </li>
+    //       ))}
+    //     </ul>
+    //   );
+    // }
+    return (
+      <div>
+          <h1>HOME page placeholder</h1>
+      </div>
+  );
 }
 
 export default TherapySearch();
