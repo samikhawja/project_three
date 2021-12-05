f3192a3418af36edf09daa9d469d94265ccd005a
Saraah Hollingsworth <sahhollingsworth@gmail.com>
2021-12-05 12:55:19 -0800

    Client-side mutation gql for user login and create

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,2 +1,36 @@
 import { gql } from '@apollo/client';
 console.log(gql);
+
+//LOGIN needs to have the same elements as the LoginForm, server/typeDefs, server/Resolvers
+export const LOGIN = gql`
+    mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+        token
+        user {
+        _id
+        }
+    }
+    }
+    `;
+
+//CREATE_USER needs to have the same elements as the SignupForm, server/typeDefs, server/Resolvers.
+export const CREATE_USER = gql`
+    mutation createUser(
+        $fName: String!
+        $lName: String!
+        $email: String!
+        $password: String!
+    ) {
+        createUser(
+        fName: $firstName
+        lName: $lastName
+        email: $email
+        password: $password
+        ) {
+        token
+        user {
+            _id
+        }
+    }
+}
+`;
