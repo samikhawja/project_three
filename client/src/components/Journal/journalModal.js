3649a58ac87dbd3eac66db4700f661212d205915
Alix17 <wildflowerinabox@gmail.com>
2021-12-04 17:01:23 -0800

    created list of journal entries in modal

--- client/src/components/Journal/journalModal.js
+++ client/src/components/Journal/journalModal.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { Modal } from "react-router-dom";
-
+import { Modal, Tab, ListGroup, Col, Row, Button } from "react-router-dom";
+//import fname from user.js
+//import entryDate, entryContent, entryTitle title,body sentiment?
 const myStyle = {
   color: "#403F48",
   backgroundColor: "#E3D9CA",
@@ -17,12 +18,40 @@ function JournalModal(props) {
     >
       <Modal.Header closeButton>
         <Modal.Title id="contained-modal-title-vcenter">
-          {entryTitle}
+          fname Journal
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <h4>{entryDate}</h4>
-        <p>{entryContent}</p>
+        <Tab.Container
+          style={myStyle}
+          id="list-group-tabs-example"
+          defaultActiveKey="#link1"
+        >
+          <Row>
+            <Col sm={4}>
+              <ListGroup>
+                <ListGroup.Item action href="#link1">
+                  entryTitle
+                </ListGroup.Item>
+                <ListGroup.Item action href="#link2">
+                  entryTitle
+                </ListGroup.Item>
+              </ListGroup>
+            </Col>
+            <Col sm={8}>
+              <Tab.Content>
+                <Tab.Pane eventKey="#link1">
+                  <h4> entryDate</h4>
+                  <p>entryContent</p>
+                </Tab.Pane>
+                <Tab.Pane eventKey="#link2">
+                  <h4>entryDate</h4>
+                  <p>entryContent</p>
+                </Tab.Pane>
+              </Tab.Content>
+            </Col>
+          </Row>
+        </Tab.Container>
       </Modal.Body>
       <Modal.Footer>
         <Button onClick={props.onHide}>Close</Button>
