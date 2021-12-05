991aa3ed5c1feed10863275cf351e7cfab3ce9e7
Alix17 <wildflowerinabox@gmail.com>
2021-12-04 18:35:53 -0800

    Journal main page done

--- client/src/components/Journal/Journal.js
+++ client/src/components/Journal/Journal.js
@@ -1,2 +1,29 @@
-//contains form + view
+import React from "react";
+import { Container, Row, Col, Button } from "react-router-dom";
+//import journalview, journalform
+//see more, submit buttons
+
+const myStyle = {
+  color: "#403F48",
+  backgroundColor: "#E3D9CA",
+};
+
+function JournalContainer() {
+  return (
+    <Container style={myStyle}>
+      <Row>
+        <Col md={6}>
+          JOURNALVIEW <Button variant="outline-success">See more</Button>{" "}
+        </Col>
+        <Col md={6}>
+          JOURNALFORM <Button variant="outline-success">Submit</Button>{" "}
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default JournalContainer;
+
+//Alix's notes
 //"see more" when clicked replaces form + view with history.js
