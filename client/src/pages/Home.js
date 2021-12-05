8712d7fc8fe7a0a1a81b01682eee08a1b5bea1a9
Alix17 <wildflowerinabox@gmail.com>
2021-12-05 15:51:21 -0800

    framework for welcome carousel and quote api to go into

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,65 @@
-import React from 'react';
+import React from "react";
+import { Carousel, Card } from "react-bootstrap";
+
+const myStyle = {
+  color: "#403F48",
+  backgroundColor: "#E3D9CA",
+};
 
 // Placeholder so the app will build
 const Home = () => {
-    return (
-        <div>
-            <h1>HOME page placeholder</h1>
-        </div>
-    );
+  return (
+    <div style={myStyle}>
+      <Carousel>
+        <Carousel.Item interval={1000}>
+          <img
+            className="d-block w-100"
+            src="holder.js/800x400?text=First slide&bg=373940"
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={500}>
+          <img className="d-block w-100" src="./logo.png" alt="Second slide" />
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="holder.js/800x400?text=Third slide&bg=20232a"
+            alt="Third slide"
+          />
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+      <Card style={myStyle}>
+        <Card.Header>Quote</Card.Header>
+        <Card.Body>
+          <blockquote className="blockquote mb-0">
+            <p>
+              {" "}
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+              posuere erat a ante.{" "}
+            </p>
+            <footer className="blockquote-footer">
+              Someone famous in <cite title="Source Title">Source Title</cite>
+            </footer>
+          </blockquote>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 };
 
 export default Home;
