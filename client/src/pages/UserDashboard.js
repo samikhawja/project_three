9b7ff976acbbdfd1a0509c73a319d097e3f5ba89
Alix17 <wildflowerinabox@gmail.com>
2021-12-06 01:37:45 -0800

    user dash page done, just needs imported links

--- client/src/pages/UserDashboard.js
+++ client/src/pages/UserDashboard.js
@@ -9,6 +9,7 @@ import CalendarPhoto from "./../assets/images/5.png";
 const myStyle = {
   color: "#403F48",
   backgroundColor: "#E3D9CA",
+  paddingBottom: "75px",
 };
 const myStyle2 = {
   color: "#403F48",
@@ -25,8 +26,14 @@ const UserDashboard = () => {
           <Card>
             <Card.Img variant="top" src={JournalPhoto} />
             <Card.Body style={myStyle}>
-              <Card.Title style={myStyle2}>Card title</Card.Title>
-              <Card.Text>Dashboard stuff. LINK</Card.Text>
+              <Card.Title style={myStyle2}>LINK JOURNAL HERE</Card.Title>
+              <Card.Text>
+                Journaling is simply writing down your thoughts and feelings to
+                understand them more clearly. If you struggle with stress,
+                depression, or anxiety, keeping a journal can be a great idea.
+                It can help you gain control of your emotions and improve your
+                mental health.
+              </Card.Text>
             </Card.Body>
           </Card>
         </Col>
@@ -34,8 +41,14 @@ const UserDashboard = () => {
           <Card>
             <Card.Img variant="top" src={TherapyPhoto} />
             <Card.Body style={myStyle}>
-              <Card.Title style={myStyle2}>Card title2</Card.Title>
-              <Card.Text>Dashboard stuff. LINK</Card.Text>
+              <Card.Title style={myStyle2}>Therapy Page link</Card.Title>
+              <Card.Text>
+                The overall goal of mental health therapy is to help you better
+                understand and manage how to overcome life's challenges. Whether
+                you are experiencing obstacles you believe to be minor or
+                significant, it's never too soon to work towards your life goals
+                in a constructive way.
+              </Card.Text>
             </Card.Body>
           </Card>
         </Col>
@@ -43,19 +56,30 @@ const UserDashboard = () => {
       <Row>
         <Col>
           <Card>
-            <Card.Img variant="top" src={ResourcesPhoto} />
+            <Card.Img variant="top" src={CalendarPhoto} />
             <Card.Body style={myStyle}>
-              <Card.Title style={myStyle2}>Card title3</Card.Title>
-              <Card.Text>Dashboard stuff. LINK</Card.Text>
+              <Card.Title style={myStyle2}>Calendar link</Card.Title>
+              <Card.Text>
+                Calendars help us cope with change, create healthy habits,
+                improve interpersonal relationships, and reduce stress. Studies
+                have shown that daily routines have far-reaching mental health
+                benefits, from alleviating bipolar disorder and preventing
+                substance abuse to managing the symptoms of other mental
+                disorders.
+              </Card.Text>
             </Card.Body>
           </Card>
         </Col>
         <Col>
           <Card>
-            <Card.Img variant="top" src={CalendarPhoto} />
+            <Card.Img variant="top" src={ResourcesPhoto} />
             <Card.Body style={myStyle}>
-              <Card.Title style={myStyle2}>Card title4</Card.Title>
-              <Card.Text>LINK</Card.Text>
+              <Card.Title style={myStyle2}>Resources Link</Card.Title>
+              <Card.Text>
+                These services often include counseling, therapy, medication
+                management, social work services, peer supports, and substance
+                use disorder treatment.
+              </Card.Text>
             </Card.Body>
           </Card>
         </Col>
