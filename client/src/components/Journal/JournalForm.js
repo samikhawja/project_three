5f5979ac3d92f80d6ba7d809321d84e266ea5ea6
Alix17 <wildflowerinabox@gmail.com>
2021-12-07 00:13:24 -0800

    handle form submit

--- client/src/components/Journal/JournalForm.js
+++ client/src/components/Journal/JournalForm.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
+import { useMutation } from "@apollo/client";
+import { CREATE_JOURNAL } from "../../utils/mutations.js";
 
 const myStyle = {
   color: "#403F48",
@@ -7,25 +9,67 @@ const myStyle = {
 };
 
 const JournalForm = () => {
+  const [formData, setFormData] = useState({
+    title: "",
+    body: "",
+    // sentiment: "",
+  });
+
+  const [createJournal] = useMutation(CREATE_JOURNAL);
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleFormSubmit = async (event) => {
+    console.log(formData);
+    event.preventDefault();
+
+    try {
+      const { data } = await createJournal({
+        variables: { ...formData },
+      });
+    } catch (err) {
+      console.log("error!");
+    }
+    setFormData({
+      title: "",
+      body: "",
+    });
+  };
+
   return (
     <Form style={myStyle}>
       <Form.Group className="mb-3" controlId="entryTitle">
-        <Form.Label>Title</Form.Label>
+        <Form.Label type="title" name="title" value={formData.title}>
+          Title
+        </Form.Label>
         <Form.Control type="entryTitle" placeholder="Todays Entry" />
       </Form.Group>
       <Form.Group className="mb-3" controlId="entryContent">
-        <Form.Label>How are you feeling today?</Form.Label>
+        <Form.Label
+          name="body"
+          onChange={handleInputChange}
+          value={formData.body}
+        >
+          How are you feeling today?
+        </Form.Label>
         <Form.Control as="textarea" rows={3} />
       </Form.Group>
-      <Button variant="outline-success">Submit</Button>{" "}
+      <Button
+        onChange={handleInputChange}
+        onSubmit={handleFormSubmit}
+        variant="outline-success"
+        type="submit"
+      >
+        Submit
+      </Button>{" "}
     </Form>
   );
 };
 
 export default JournalForm;
-//Alix's notes
-//needs styling
-// submitEntry needs to do something
-// needs 3 checkboxes after checking how it renders
-// happy, ambivalent, sad
-//form helpers from https://react-bootstrap.github.io/components/forms/
