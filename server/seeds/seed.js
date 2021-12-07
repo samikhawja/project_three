d9e1b021052744df1304d91a0605223cd22b6b52
Saraah Hollingsworth <sahhollingsworth@gmail.com>
2021-12-06 17:34:32 -0800

    Updated Seed logic to match updated schemas

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,24 +1,50 @@
 const db = require('../config/connection');
 const { User, Journal } = require('../models');
-const userSeeds = require('./userSeeds.json');
 const journalSeeds = require('./journalSeeds.json');
 
 db.once('open', async () => {
     try {
-        await Journal.deleteMany({});
-        await User.deleteMany({});
-        await User.create(userSeeds);
-        for (let i = 0; i < journalSeeds.length; i++) {
-            const { _id, title } = await Journal.create(journalSeeds[i]);
-            const user = await User.findOneAndUpdate(
-                { email: title },
-                {
-                $addToSet: {
-                    journals: _id,
-                },
-            }
-        );
-        }
+        await Journal.deleteMany();
+
+        const journalSeed = await Journal.insertMany(journalSeeds);
+
+        console.log('Journals seeded');
+        
+        await User.deleteMany();
+
+        await User.create({
+            "email": "skhawja@gmail.com",
+            "password": "password1",
+            "fname": "Sami",
+            "lname": "Khawja",
+            "journals": [journalSeed[6]._id, journalSeed[7]._id]
+        });
+
+        await User.create({
+            "email": "afriedman@gmail.com",
+            "password": "password2",
+            "fname": "Alix",
+            "lname": "Friedman",
+            "journals": [journalSeed[0]._id, journalSeed[1]._id, journalSeed[2]._id, journalSeed[3]._id, journalSeed[4]._id, journalSeed[5]._id,]
+        });
+
+        await User.create({
+            "email": "sarah@gmail.com",
+            "password": "qwert12345",
+            "fname": "Sarah",
+            "lname": "Hollingsworth",
+            "journals": [journalSeed[8]._id, journalSeed[9]._id, journalSeed[10]._id, journalSeed[11]._id]
+        });
+
+        await User.create({
+            "email": "johnsmoth@gmail.com",
+            "password": "password3",
+            "fname": "John",
+            "lname": "Smith",
+        });
+
+        console.log('Users seeded');
+        
     } catch (err) {
         console.error(err);
         process.exit(1);
