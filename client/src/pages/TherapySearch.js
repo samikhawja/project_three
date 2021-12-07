0f53ec3873e044d3a25f2bfaf4d1119d278ebc68
samikhawja <skhawja11@gmail.com>
2021-12-06 22:00:35 -0800

    added parse for api

--- client/src/pages/TherapySearch.js
+++ client/src/pages/TherapySearch.js
@@ -5,26 +5,37 @@ import {
   FloatingLabel,
   Container,
   Button,
-  ListGroup,
 } from "react-bootstrap";
 import "./../App.css";
-import React from "react";
-//import useFetch from "react-fetch-hook";
+import React, { useEffect } from "react";
+import { useLazyQuery } from "@apollo/client";
+import { SEARCH } from "../utils/queries";
 
-// require('dotenv').config();
 const myStyle = {
   color: "#403F48",
   backgroundColor: "#E3D9CA",
   width: "100%",
   height: "100%",
-  paddingBottom: "75px",
 };
 function TherapySearch() {
-  // const { isLoading, error, data } = useFetch("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=37.759185,-122.427304&radius=40235&types=doctor&name=addiction_therapy&key=AIzaSyC23nfE1PhzLbG546fR0JtV9PrwJg8iyMY%22)
-
-  // if (isLoading) return "Loading...";
-  // if (error) return "Error!";
-  // console.log(data.results);
+  const [getSearch, {loading, error, data}] = useLazyQuery(SEARCH)
+  useEffect(() => {
+    if(data) {
+      try {const result = JSON.parse(data.searchTherapy.result)
+        console.log(result)
+        console.log("it's parsed")
+      }
+      catch (error){
+        console.log(data.searchTherapy.result)
+        console.error(error)
+      }
+    }
+  }, [data])
+  function beginSearch (){
+    getSearch()
+  }
+  if (error) return ("Error", error)
+  if (loading) return "Loading...";
 
   return (
     <Container style={myStyle}>
@@ -36,41 +47,24 @@ function TherapySearch() {
               <option>Options</option>
               <option value="1">AA Support Group</option>
               <option value="2">Therapists Nearby</option>
-              <option value="3">Rehab Groups</option>
+              <option value="3">Rehab Facilities</option>
             </Form.Select>
           </FloatingLabel>
           <Form>
             <Row>
-              <FloatingLabel
-                style={myStyle}
-                controlId="floatingSelect"
-                label="Choose an option"
-              >
-                <Form.Select aria-label="Floating label select example">
-                  <option>Distance</option>
-                  <option value="1">miles</option>
-                  <option value="2">miles</option>
-                  <option value="3">miles</option>
-                </Form.Select>
-              </FloatingLabel>
+              <Col xs={7}>
+                <Form.Control placeholder="Within (  )" />
+              </Col>
+              <Col>
+                <Form.Control placeholder="Within (  )" />
+              </Col>
+              <Col>
+                <Form.Control placeholder="Within (  )" />
+              </Col>
             </Row>
-            <Button variant="success">Submit</Button>{" "}
+            <Button variant="success" onClick={beginSearch}>Submit</Button>{" "}
             <Row>
-              <Col>INSERT GOOGLE API RESPONSE HERE</Col>
-              <ListGroup>
-                <ListGroup.Item action href="#link1">
-                  therapist results
-                  <Button variant="success">Add Therapist</Button>{" "}
-                </ListGroup.Item>
-                <ListGroup.Item action href="#link2">
-                  group results
-                  <Button variant="success">Add AA Support Group</Button>{" "}
-                </ListGroup.Item>
-                <ListGroup.Item action href="#link3">
-                  group results
-                  <Button variant="success">Add support Group</Button>{" "}
-                </ListGroup.Item>
-              </ListGroup>
+              <Col>Hello World</Col>
             </Row>
           </Form>
         </div>
