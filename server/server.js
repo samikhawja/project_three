ce8c1f385467f6d7173a7375ea1ece3367ed520d
samikhawja <skhawja11@gmail.com>
2021-12-07 09:57:35 -0800

    changing if, to will do

--- server/server.js
+++ server/server.js
@@ -20,9 +20,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 app.use(routes)
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
-}
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build/index.html'));
